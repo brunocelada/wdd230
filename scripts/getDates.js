978287d473b09f3433e2a25f62d1e5b891565fec
Bruno Celada <brunocelada-4@hotmail.com>
2023-09-27 10:05:17 -0300

    L03: Home Page Enhancement

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,12 @@
+// Dates
 document.querySelector("#current-year").innerHTML = new Date().getFullYear();
 
 document.querySelector("#lastModified").innerHTML = new Date(
   document.lastModified
 );
+
+// Click Menu event listender (hamburger button)
+document.querySelector("#menu").addEventListener("click", () => {
+  document.querySelector(".navigation").classList.toggle("show");
+  document.querySelector("#menu").classList.toggle("show");
+});
