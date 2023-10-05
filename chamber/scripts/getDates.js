f87eab3ff4d04234a00f32f8aedef76bcf7ee235
Bruno Celada <brunocelada-4@hotmail.com>
2023-10-05 09:35:01 -0300

    first steps of the chamber site

---
+++ chamber/scripts/getDates.js
@@ -0,0 +1,12 @@
+// Dates
+document.querySelector("#current-year").innerHTML = new Date().getFullYear();
+
+document.querySelector("#lastModified").innerHTML = "Last modified: " + new Date(
+  document.lastModified
+);
+
+// Click Menu event listender (hamburger button)
+document.querySelector("#menu").addEventListener("click", () => {
+  document.querySelector(".navigation").classList.toggle("show");
+  document.querySelector("#menu").classList.toggle("show");
+});
